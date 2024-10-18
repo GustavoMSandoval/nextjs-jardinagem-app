f22d4cabb85ae32d62f7bf7580a2605b32bccac3
GustavoMSandoval <gmsandoval2005@gmail.com>
2024-10-18 15:47:01 -0300

    fix: landing pages

--- src/components/layout/landing-page-projects.tsx
+++ src/components/layout/landing-page-projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function LandingPageAbout() {
+export default function LandingPageProjects() {
   return (
     <div></div> 
   )
--- src/components/layout/landing-page-terceirizacao.tsx
+++ src/components/layout/landing-page-terceirizacao.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-export default function LandingPageAbout() {
+export default function LandingPageTerceirizacao() {
   return (
     <div></div> 
   )
 }
+
